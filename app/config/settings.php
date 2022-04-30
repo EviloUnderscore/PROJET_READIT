1a82cf87d458a781f04dd8b80710b483a645f720
evilo <olivier.ledc@gmail.com>
2022-04-30 16:20:17 +0200

    Structure MVC vierge
    mise en place d'une structure MVC vierge

---
+++ app/config/settings.php
@@ -0,0 +1,8 @@
+<?php
+$content = '';
+$title = '';
+
+define('HOSTNAME', 'localhost:3306');
+define('DBNAME', 'readit_2020');
+define('USERNAME', 'root');
+define('USERPWD', 'root');
---
+++ core/init.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once '../app/config/settings.php';
+require_once '../core/connexion.php';
