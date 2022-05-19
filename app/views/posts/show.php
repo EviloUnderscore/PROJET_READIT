37be021b1558fc322a0999b00b560b310bfaffca
evilo <olivier.ledc@gmail.com>
2022-05-19 15:46:41 +0200

    Details d'un post - liste TAGS

--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -12,3 +12,11 @@ $post : ARRAY(id, title, created_at, resume, image, content, author_id, categori
     <div><?php echo $post['content'];?></div>
         </div>
     </div>
+
+<?php 
+
+    include_once '../app/models/tagsModel.php';
+    $tags = \App\Models\TagsModel\findAllByPostId($connexion, $post['id']);
+    include '../app/views/tags/_indexByPostId.php';
+
+?>
